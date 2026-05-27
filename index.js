c19377466f6f158580251901f2b69ca8e415f790
Dhruva Maheshwari <maheshwaridhruva0@gmail.com>
2026-05-27 12:36:19 +0530

    add line in index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express()
 const port = 4000;
 
 app.get('/' , (req  ,res) => {
-    res.send('hello kya haal h')
+    res.send('hello kya haal h keso ho')
 })
 
 app.listen(port , () => console.log(`server is running on port ${port}`))
